7258f79f9601c96bad75c393e482be7237e84ea9
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-09-11 16:51:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement stack using array/implement-stack-using-array.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.util.*;
+
+// Define MyStack class here
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        scanner.nextLine(); // Flush the newline character left by nextInt
+        while (T-- > 0) {
+            MyStack sq = new MyStack();
+            String line = scanner.nextLine();
+            Scanner ss = new Scanner(line);
+            List<Integer> nums = new ArrayList<>();
+            while (ss.hasNextInt()) {
+                int num = ss.nextInt();
+                nums.add(num);
+            }
+            int n = nums.size();
+            int i = 0;
+            while (i < n) {
+                int QueryType = nums.get(i++);
+                if (QueryType == 1) {
+                    int a = nums.get(i++);
+                    sq.push(a);
+                } else if (QueryType == 2) {
+                    System.out.print(sq.pop() + " ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class MyStack {
+    private int[] arr;
+    private int top;
+
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    }
+
+    public void push(int x) {
+        // Your Code
+        // I have to push the element into the stack
+       top = top + 1;
+       arr[top] = x;
+    }
+
+    public int pop() {
+        // Your Code
+        // It returns an integer present at the top and popped out from the stack.
+        // I should return the element that is popped out in the current operation
+        if(top == -1){
+            return top;
+        }
+        top = top - 1;
+        return arr[top+1];
+    }
+}
