f842a60694c2c17ae50618f3f9237150861c2a78
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-09-28 23:02:11 +0530

    Time: 6 ms (65.93%), Space: 45.7 MB (7.08%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> remainingAsterioids = new ArrayDeque<>();
+
+        for (int i = 0; i < asteroids.length; i++) {
+            int asteroid = asteroids[i];
+
+            if (remainingAsterioids.isEmpty()) {
+                remainingAsterioids.add(asteroid);
+                continue;
+            }
+            int prevAsteroid = remainingAsterioids.peekLast();
+            remainingAsterioids.add(asteroid);
+            if (asteroidsWillCollide(prevAsteroid, asteroid))
+                destoryAsteroids(remainingAsterioids);
+        }
+
+        return fromDequeToArray(remainingAsterioids);
+    }
+
+    private int[] fromDequeToArray(Deque<Integer> nums) {
+        int[] arr = new int[nums.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = nums.pollFirst();
+        }
+        return arr;
+    }
+
+    private void destoryAsteroids(Deque<Integer> asteroids) {
+        while (asteroids.size() > 1) {
+            int last = asteroids.pollLast();
+            int secondLast = asteroids.pollLast();
+
+            if (!asteroidsWillCollide(secondLast, last)) {
+                asteroids.add(secondLast);
+                asteroids.add(last);
+                break;
+            }
+            if (Math.abs(last) > Math.abs(secondLast))
+                asteroids.add(last);
+            else if (Math.abs(secondLast) > Math.abs(last))
+                asteroids.add(secondLast);
+        }
+    }
+
+    private boolean asteroidsWillCollide(int before, int after) {
+        return before > 0 && after < 0;
+    }
+}
